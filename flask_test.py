144dd81cde69e52e4a4c1746c9deae6eb2895075
Emilb05 <emilhal21@tskoli.is>
2024-03-13 15:47:22 +0000

    jebb

--- flask_test.py
+++ flask_test.py
@@ -4,6 +4,7 @@ app = Flask(__name__)# more test
 
 @app.route("/")
 def hello_world():# more tests
+    # issue?
     return "<center><p>Emil Berg Halldórsson, sem er bestur í heimi</p><center>"
 print("let's start running")# fun
 app.run()# yea, more tests
